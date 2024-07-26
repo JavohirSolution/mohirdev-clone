e76dc980ba77e668678814984c1562c4ec7bb8e7
JavohirSolution <javohirdeveloper12@gmail.com>
2024-07-26 18:10:01 +0500

    add .env

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDsdxz9mB045MSITVsefcCpV71-KsnSUeI",
-    authDomain: "mohirdev-clone-react.firebaseapp.com",
-    projectId: "mohirdev-clone-react",
-    storageBucket: "mohirdev-clone-react.appspot.com",
-    messagingSenderId: "28238546831",
-    appId: "1:28238546831:web:1e56c2352d4fd6e3f86dfa",
-    measurementId: "G-QQR2V1WEVD"
+    apiKey: process.env.FIREBASE_KEY,
+    authDomain: process.env.FIREBASE_AUTHDOMAIN,
+    projectId: process.env.FIREBASE_PROJECTID,
+    storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGEINSENDERID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENTID,
 };
 
 // Initialize Firebase
